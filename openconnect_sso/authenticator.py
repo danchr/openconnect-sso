6ef1ab3e6217f3c8753c16eefec9eed0a0796ba3
Dan Villiom Podlaski Christiansen <danchr@gmail.com>
2023-02-28 16:22:46 +0100

    Don't fail, but issue a warning when detecting redirect fails

--- openconnect_sso/authenticator.py
+++ openconnect_sso/authenticator.py
@@ -57,10 +57,14 @@ class Authenticator:
     def _detect_authentication_target_url(self):
         # Follow possible redirects in a GET request
         # Authentication will occcur using a POST request on the final URL
-        response = self.session.get(self.host.vpn_url)
-        response.raise_for_status()
-        self.host.address = response.url
         logger.debug("Auth target url", url=self.host.vpn_url)
+        try:
+            response = self.session.get(self.host.vpn_url)
+            response.raise_for_status()
+            self.host.address = response.url
+        except Exception:
+            logger.warn("Failed to check for redirect")
+            self.host.address = self.host.vpn_url
 
     def _start_authentication(self):
         request = _create_auth_init_request(self.host, self.host.vpn_url)
